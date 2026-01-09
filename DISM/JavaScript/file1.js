737b1cf466f42c6f3a2488b8bfbaa640d3e65d93
farazinam <farazinam17@gmail.com>
2026-01-09 17:10:52 +0500

    Destructuring, Spread, Rest, Classes and Objects, Import and Export

---
+++ DISM/JavaScript/file1.js
@@ -0,0 +1,4 @@
+import {PI, calculate} from './file2.js'
+
+console.log(PI);
+console.log(calculate(12, 7));
---
+++ DISM/JavaScript/file2.js
@@ -0,0 +1,6 @@
+export const PI = 3.14;
+
+export function calculate(a, b){
+    return a + b
+}
+
