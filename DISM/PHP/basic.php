d5ab7e0c5a79bb2acf16a1d2ef255411bfdca33a
farazinam <farazinam17@gmail.com>
2026-02-20 14:35:30 +0500

    Multi-dimentional and Associative Array

--- DISM/PHP/basic.php
+++ DISM/PHP/basic.php
@@ -220,18 +220,76 @@ print("<hr>");
    echo("<h2> Array </h2>");
    echo("<h3> Indexed </h3>");
 
-   $ind = array("A", "B", "C");
-   var_dump($ind);
-   echo($ind[0]);
-   echo($ind[1]);
-   echo($ind[2]);
+   //Method 1
+   $ind1 = array("A", "B", "C");
+   var_dump($ind1);
+
+   //Method 2
+   $ind2 = [2, 4, 6, 8, 10];
+   echo($ind2[0]);
+   echo($ind2[1]);
+   echo($ind2[2]);
+
+   print("<br>");
+
+   print_r($ind1);
+
+   print("<br>");
+
+   echo($ind1[0]);
+   echo($ind1[1]);
+   echo($ind1[2]);
+
+   print("<br>");
 
    for($a = 0; $a < 3; $a++){
-      echo($ind[$a]);
+      echo($ind1[$a]);
+   }
+   print("<br>");
+
+   foreach($ind2 as $data){
+      echo($data);
    }
 
-   echo("<h3> Multi-Dimentio </h3>");
+   echo("<h3> Multi-Dimentional </h3>");
 
+   $md1 = [
+      ["Ali", "Ahmed", "Arham"],
+      [1, 3, 5, 7, 9],
+      [true, false],
+      [
+         [2.5, 7.9, 8.3],
+         [2]
+      ]
+   ];
+
+   echo($md1[1][3]);
+   echo($md1[3][0][1]);
+
+
+   echo("<h3> Associative </h3>");
+
+   $aa = Array(
+      "name" => "Ali",
+      "age" => 23,
+      "city" => "Karachi"
+   );
+   
+   echo($aa["name"]);
+   echo($aa["age"]);
+   echo($aa["city"]);
+   
+   print("<br>");
+
+   foreach($aa as $k){
+      echo($k);
+   }
+
+   print("<br>");
+
+   foreach($aa as $k => $v){
+      echo($k . " " . $aa[$k]. "<br>");
+   }
 
     ?>
 </body>
