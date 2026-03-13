76c08de229e0442960e2dc44d28c8f233fff8072
farazinam <farazinam17@gmail.com>
2026-03-13 14:38:58 +0500

    Update in CRUD

--- DISM/PHP/Clothes Website/updateCategory.php
+++ DISM/PHP/Clothes Website/updateCategory.php
@@ -7,5 +7,26 @@
   $select =  "SELECT * FROM category WHERE category_id = '$getId'";
   $query = mysqli_query($conn, $select);
 
-
+  $data = mysqli_fetch_array($query);
+  // echo $data[0];
+  // echo $data[1];
 ?>
+
+  <form action="" method="POST">
+        <label for="">Category Name</label>
+        <input type="text" name="category" value="<?php echo $data[1] ?>">
+        <button type="submit" name="updcat">Update Category</button>
+    </form>
+
+    <?php
+     if(ISSET($_POST["updcat"])){
+     $cat = $_POST["category"];
+    $update = "UPDATE category SET category_name = '$cat' WHERE category_id = '$getId'";
+    $query = mysqli_query($conn, $update);
+
+    if($query){
+            echo "Record Updated!";
+        }
+   }
+    
+    ?>
