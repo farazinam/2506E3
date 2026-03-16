b13d349309f478e9ebcbc4a9b10dca679286a20e
farazinam <farazinam17@gmail.com>
2026-03-16 14:33:39 +0500

    Template Embedding (Admin)

---
+++ DISM/PHP/Clothes Website/admin/addCategory.php
@@ -0,0 +1,29 @@
+<?php 
+include("header.php");
+?>
+
+
+            <!-- Form Start -->
+            <div class="container-fluid pt-4 px-4">
+                <div class="row g-4">
+                    <div class="col-sm-12 col-xl-6">
+                        <div class="bg-light rounded h-100 p-4">
+                            <h6 class="mb-4">Category</h6>
+                            <form>
+                                <div class="mb-3">
+                                    <label for="exampleInputEmail1" class="form-label">Add Category</label>
+                                    <input type="text" class="form-control" id="exampleInputEmail1"
+                                        aria-describedby="emailHelp">
+                                </div>
+                                <button type="submit" class="btn btn-primary">Add Category</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Form End -->
+
+
+            <?php 
+include("footer.php");
+?>
