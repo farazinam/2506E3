7cbe797083de5a1516939a5efc05a04cef5ed04a
farazinam <farazinam17@gmail.com>
2026-06-24 16:58:39 +0500

    DDL, DML Queries and Contraints

--- HDSE-I/SQL Server/SQLServer_Queries.sql
+++ HDSE-I/SQL Server/SQLServer_Queries.sql
@@ -19,10 +19,10 @@ VALUES ('Muhammad', 'Ali', 'muhammadali@gmail.com', 3102, 'abc Street');
 INSERT INTO contact
 VALUES ('Muhammad', 'Bilal', 'muhammadbilal@gmail.com', 3102, 'abc Street');
 
---INSERT INTO contact
---VALUES ('Muhammad', 'Ibrahim', 'muhammadibrahim@gmail.com', 3102, 'abc Street'),
---('Muhammad', 'Hassan', 'muhammadhassan@gmail.com', 3103, 'abc Street'),
---('Muhammad', 'Hussain', 'muhammadhussain@gmail.com', 3104, 'abc Street');
+INSERT INTO contact
+VALUES ('Muhammad', 'Ibrahim', 'muhammadibrahim@gmail.com', 3102, 'abc Street'),
+('Muhammad', 'Hassan', 'muhammadhassan@gmail.com', 3103, 'abc Street'),
+('Muhammad', 'Hussain', 'muhammadhussain@gmail.com', 3104, 'abc Street');
 
 --INSERT INTO contact (first_name, email, address)
 --VALUES ('Muhammad', 'muhammadali@gmail.com', 'abc Street');
@@ -42,3 +42,57 @@ DROP COLUMN city;
 EXEC sp_rename 'contact.phone_number', 'contact_number', 'COLUMN';
 
 
+ -- ------------------ Day 2 ---------------
+
+SELECT * FROM contact;
+
+ -- Drop
+
+DROP TABLE contact;
+DROP DATABASE lab1;
+
+TRUNCATE TABLE contact;
+DELETE FROM contact;
+
+-- Rename
+EXEC sp_rename 'contact', 'rabta';
+
+
+-- Constraints
+
+CREATE TABLE customer (
+id INT PRIMARY KEY IDENTITY(1,1),
+name VARCHAR(50) NOT NULL,
+age INT NOT NULL check(age >= 18),
+city VARCHAR(50) DEFAULT 'Karachi',
+email VARCHAR(100) UNIQUE,
+contact VARCHAR (100) NOT NULL
+);
+
+INSERT INTO customer (name, age, city, email, contact)
+VALUES ('Ali', 23, 'Lahore', 'ali@gmail.com', '0310-1234567');
+
+INSERT INTO customer (name, age, city, email, contact)
+VALUES ('Ahmed', 18, 'Lahore', 'ahmed@gmail.com', '0310-1234567');
+
+INSERT INTO customer (name, age, city, contact)
+VALUES ('Ahmed', 18, 'Lahore', '0310-1234567');
+
+INSERT INTO customer (name, age, email, contact)
+VALUES ('Ahmed', 18, 'ahmed123@gmail.com', '0310-1234567');
+
+SELECT * FROM customer;
+
+-- DML Queries
+
+INSERT INTO customer (name, age, city, email, contact)
+VALUES ('Ali', 23, 'Lahore', 'ali@gmail.com', '0310-1234567');
+
+UPDATE customer set name = 'Haris' WHERE name = 'Ahmed';
+UPDATE customer set name = 'Danish' WHERE id = 5;
+
+UPDATE customer 
+set age = 19, city = 'Multan', email = 'danish@gmail.com' 
+WHERE id = 5;
+
+DELETE FROM customer WHERE  id = 8;
